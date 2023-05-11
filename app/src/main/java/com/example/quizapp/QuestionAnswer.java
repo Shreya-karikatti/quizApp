f6569cf5ea29255169850d0252469af22f6c4792
Shreya-karikatti <109196887+Shreya-karikatti@users.noreply.github.com>
2023-05-11 13:12:30 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/quizapp/QuestionAnswer.java
@@ -0,0 +1,34 @@
+package com.example.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "How many hearts does an octopus have?",
+            "What planet is closest to the sun?",
+            "How many dots appear on a pair of dice?",
+            "Which is the only body part that is fully grown from birth?",
+            "What color are Mickey Mouse's shoes?",
+            "Where is Disney's European theme park located?"
+
+    };
+
+    public static String choices[][] = {
+            {"2","3","4","5"},
+            {"Earth","Mars","Mercury","Saturn"},
+            {"52","34","46","42"},
+            {"Eyes","Kidney","Ears","Heart"},
+            {"Black","Yellow","Blue","Red"},
+            {"Marseille","Nice","Paris","Limoges"}
+    };
+
+    public static String correctAnswer[] = {
+            "3",
+            "Mercury",
+            "42",
+            "Eyes",
+            "Yellow",
+            "Paris"
+    };
+
+
+}
